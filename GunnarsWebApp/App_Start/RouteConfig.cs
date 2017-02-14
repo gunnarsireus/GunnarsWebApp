e715948d3d134bd7fccc331aec993ddd585dd044
gunnarsireus <gunnar.sireus@gmail.com>
2017-02-14 18:37:09 +0100

    Second

--- GunnarsWebApp/App_Start/RouteConfig.cs
+++ GunnarsWebApp/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace GunnarsWebApp
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "SecondRoute",
+                url: "{controller}/{action}/{id,employeeId}",
+                defaults: new { controller = "Employeee", action = "Index", id = UrlParameter.Optional }
+);
         }
     }
 }
